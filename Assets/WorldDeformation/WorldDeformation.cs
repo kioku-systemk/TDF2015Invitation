b66c30abe9099186b75b5bf080dcc59622a59038
Julien Guertault <julien.guertault@gmail.com>
2015-02-13 03:51:17 +0900

    Example of Debug.Log not working for i-saint

--- Assets/WorldDeformation/WorldDeformation.cs
+++ Assets/WorldDeformation/WorldDeformation.cs
@@ -13,5 +13,7 @@ public class WorldDeformation : MonoBehaviour {
 		var m = GetComponent<Renderer>().sharedMaterial;
 		m.SetFloat("_vertexTranslation",	parameters.vertex_translation);
 		m.SetFloat("_vertexDeformation",	parameters.vertex_deformation);
+		Debug.Log("_vertexTranslation " + parameters.vertex_translation);
+		Debug.Log("_vertexDeformation " + parameters.vertex_deformation);
 	}
 }
